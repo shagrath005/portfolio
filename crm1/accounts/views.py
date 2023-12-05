0ba77f6c6efce5c033d4ccc9a85feb33f4202de4
shagrath005 <levanmaisuradze@gmx.de>
2023-12-05 16:39:57 +0400

    crm 1

---
+++ crm1/accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+
+
+def home(request):
+	return render(request, 'accounts/dashboard.html')
+
+def products(request):
+	products = Product.objects.all()
+
+	return render(request, 'accounts/products.html',{'products':products})
+
+def customer(request):
+	return render(request, 'accounts/customer.html')
